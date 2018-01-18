f4388f996cf8af5a79d03dd05fc49ce98a1c7e9b
William George Cook <wgeorgecook@gmail.com>
2018-01-17 20:20:55 -0800

    Generic python3 venv setup script

---
+++ deployment/setup-py3-env.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Check for the lock file, if it exists, we know this vagrant VM has already been
+# provisioned and we simply want to reload everything
+
+lockfile=~/install-python3.lock
+
+if [ -e "$lockfile" ]; then
+    # Reload steps
+    echo "Reload detected, refreshing env"
+
+
+    # Cat-ing out the requirements txt to make sure it's populated
+    cat ~/requirements.txt
+    sleep 10
+
+
+    # Run pip install on requirements to pick up new packages
+    ~/python3_env/bin/pip install -r ~/deployment/requirements.txt
+else
+    # Setup the env
+    virtualenv -p /usr/bin/python3  ~/python3_env
+
+    # Cat-ing out the requirements txt to make sure it's populated
+    cat ~/deployment/requirements.txt
+    sleep 10
+
+    # Upgrade pip
+    ~/python3_env/bin/pip install --upgrade
+
+    # Pip install the requirements
+    ~/python3_env/bin/pip install -r ~/deployment/requirements.txt
+
+    # Make this env activate on log in
+    echo "source ~/python3_env/bin/activate" >> ~/.profile
+
+    # Place the lockfile
+    touch ~install-python3.lock
+    echo "Python 3 setup complete"
+fi
